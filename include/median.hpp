371b3010db7ca9490ca4af6aa2a47dc4c7a45aca
Oliver Schönrock <oliver@openbrackets.net>
2022-02-14 15:36:28 +0000

    improvements to genericity of median()

--- include/median.hpp
+++ include/median.hpp
@@ -6,30 +6,46 @@
 #include <stdexcept>
 #include <vector>
 
+template <typename RandomAccessIter, typename Comp = std::ranges::less,
+          typename Proj = std::identity>
+requires std::random_access_iterator<RandomAccessIter> &&
+    std::sortable<RandomAccessIter, Comp, Proj>
+auto median_in_place(RandomAccessIter first, RandomAccessIter last, const Comp& comp = {},
+                     const Proj& proj = {}) {
+
+  auto size = std::distance(first, last);
+  if (size == 0) throw std::domain_error("Can't find median of an empty range.");
+
+  auto middle = first + (size / 2);
+
+  std::ranges::nth_element(first, middle, last, comp, proj);
+
+  if (size % 2 == 1) return std::invoke(proj, *middle);
+
+  auto below_middle = std::ranges::max_element(first, middle, comp, proj);
+
+  return std::midpoint(std::invoke(proj, *middle), std::invoke(proj, *below_middle));
+}
+
+template <typename RandomAcccessRange, typename Comp = std::less<>, typename Proj = std::identity>
+requires std::ranges::random_access_range<RandomAcccessRange> &&
+    std::sortable<std::ranges::iterator_t<RandomAcccessRange>, Comp, Proj>
+auto median_in_place(RandomAcccessRange& range, const Comp& comp = {}, const Proj& proj = {}) {
+  return median_in_place(std::ranges::begin(range), std::ranges::end(range), comp, proj);
+}
+
 template <typename InputIter, typename Comp = std::ranges::less, typename Proj = std::identity>
 requires std::input_iterator<InputIter> &&
     std::sortable<typename std::vector<std::iter_value_t<InputIter>>::iterator, Comp, Proj>
-auto median(InputIter first, InputIter last, Comp comp = {}, Proj proj = {}) {
-
-  std::vector<std::iter_value_t<InputIter>> tmp(first, last); // always make a copy
-
-  if (tmp.empty()) throw std::domain_error("Can't find median of an empty range.");
-
-  auto tmp_middle = tmp.begin() + (tmp.size() / 2);
-
-  std::ranges::nth_element(tmp.begin(), tmp_middle, tmp.end(), comp, proj);
-
-  if (tmp.size() % 2 == 1) return std::invoke(proj, *tmp_middle);
-
-  auto below_tmp_middle = std::ranges::max_element(tmp.begin(), tmp_middle, comp, proj);
-
-  return std::midpoint(std::invoke(proj, *tmp_middle), std::invoke(proj, *below_tmp_middle));
+auto median(InputIter first, InputIter last, const Comp& comp = {}, const Proj& proj = {}) {
+  std::vector<std::iter_value_t<InputIter>> input_copy(first, last); // always make a copy
+  return median_in_place(input_copy, comp, proj);
 }
 
 template <typename InputRange, typename Comp = std::less<>, typename Proj = std::identity>
 requires std::ranges::input_range<InputRange> &&
     std::sortable<typename std::vector<std::ranges::range_value_t<InputRange>>::iterator, Comp,
                   Proj>
-auto median(InputRange range, Comp comp = {}, Proj proj = {}) {
-  return median(range.begin(), range.end(), comp, proj);
+auto median(const InputRange& range, const Comp& comp = {}, const Proj& proj = {}) {
+  return median(std::ranges::begin(range), std::ranges::end(range), comp, proj);
 }
