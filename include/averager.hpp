30cd5a8e859b484c65f7586bd7e02a058d779ddb
Oliver Schönrock <oliver@openbrackets.net>
2022-02-14 20:53:23 +0000

    running averager

---
+++ include/averager.hpp
@@ -0,0 +1,21 @@
+template <typename T>
+struct averager {
+    T sum{};
+    T avg{};
+
+    unsigned       sample_count = 0;
+    const unsigned max_samples;
+
+    explicit averager(unsigned max_samples_) : max_samples(max_samples_) {}
+
+    T operator()(T new_value) {
+        if (sample_count != max_samples) {
+            sum += new_value;
+            ++sample_count;
+        } else {
+            sum += new_value - avg;
+        }
+        avg = sum / sample_count;
+        return avg;
+    }
+};
