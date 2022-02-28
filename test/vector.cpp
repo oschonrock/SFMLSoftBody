84891a6f99afb258971cac6fb4922d3160f81596
Oliver Schönrock <oliver@openbrackets.net>
2022-02-28 22:41:02 +0000

    starting to implement vector2 testing

--- test/vector.cpp
+++ test/vector.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
-
-#include "Point.hpp"
 #include "Vector2.hpp"
+#include "gtest/gtest.h"
 
-constexpr float radius = 0.05F;
 
-int main() {
-    std::cout << Vec2(0,1) - Vec2(0,2) << '\n'; // [0, 1]
-    std::cout << (Vec2(0,0) - Vec2(1,1)).mag() << '\n'; // 1.41421
-    std::cout << Vec2(1,1).dot(Vec2(2,2)) << '\n'; // 4
-    std::cout << Vec2(1,1).dot(Vec2(2,-2)) << '\n'; // 0
-    std::cout << Vec2(2,2).norm() << '\n'; // 0
-
-    Point p1(Vec2(3, 3), 1.0, radius);
-    Point p2(Vec2(3, 3.2), 1.0, radius);
-    Point::springHandler(p1, p2, 1, 8000, 100);
-    std::cout << p1.f << '\n';
+TEST(vector2, constructor) {
+    EXPECT_EQ(Vec2(), Vec2(0, 0));
 }
+TEST(vector2, addition) {
+    EXPECT_EQ(Vec2I(1,2) + Vec2I(10,20), Vec2(11, 22));
+}
+// int main() {
+//     std::cout << Vec2(0,1) - Vec2(0,2) << '\n'; // [0, 1]
+//     std::cout << (Vec2(0,0) - Vec2(1,1)).mag() << '\n'; // 1.41421
+//     std::cout << Vec2(1,1).dot(Vec2(2,2)) << '\n'; // 4
+//     std::cout << Vec2(1,1).dot(Vec2(2,-2)) << '\n'; // 0
+//     std::cout << Vec2(2,2).norm() << '\n'; // 0
+
+// }
